0543544bb3f8e1ab5007ad472c39c7b446c0eaca
mkhegde <149266038+mkhegde@users.noreply.github.com>
2025-09-21 00:42:50 +0100

    Add Tailwind CSS configuration file

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{ts,tsx,mdx}",
+    "./components/**/*.{ts,tsx,mdx}",
+    "./calculators/**/*.{ts,tsx,mdx}",
+    "./data/**/*.{ts,tsx,mdx}",
+    "./styles/**/*.{css}"
+  ],
+  theme: { extend: {} },
+  plugins: [],
+};
